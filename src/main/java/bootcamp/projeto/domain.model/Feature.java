a7c75a9412739f1743cfa4722d1592520c243f06
Mislaine <a.s.mislaine@gmail.com>
2023-09-26 13:41:22 -0300

    annotations

--- src/main/java/bootcamp/projeto/domain.model/Feature.java
+++ src/main/java/bootcamp/projeto/domain.model/Feature.java
@@ -1,6 +1,14 @@
 package bootcamp.projeto.domain.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity(name = "tb_feature")
 public class Feature {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String icon;
     private String description;
--- src/main/java/bootcamp/projeto/domain.model/User.java
+++ src/main/java/bootcamp/projeto/domain.model/User.java
@@ -1,13 +1,22 @@
 package bootcamp.projeto.domain.model;
 
+import jakarta.persistence.*;
+
 import java.util.List;
+@Entity(name = "tb_user")
 
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+    @OneToOne(cascade = CascadeType.ALL)// todas as operações de persistência (inserir, atualizar, excluir) em uma entidade são propagadas automaticamente para a entidade associada.
     private Account account;
+    @OneToOne(cascade = CascadeType.ALL)
     private Card card;
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER) //é uma estratégia de busca (fetch strategy) usada em ORM,quando buscar uma instância da classe principal que possui uma associação @OneToOne, o JPA também carrega automaticamente a entidade associada.
     private List<Feature> featureList;
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
     private List<News> newsList;
 
     public Long getId() {
