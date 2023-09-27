9d80bcb93df24bebd84f709510baf1fcc5e43db9
Mislaine <a.s.mislaine@gmail.com>
2023-09-27 14:57:56 -0300

    dev.properties

--- src/main/java/bootcamp/projeto/BootcampSantander2023Application.java
+++ src/main/java/bootcamp/projeto/BootcampSantander2023Application.java
@@ -3,11 +3,15 @@ package bootcamp.projeto;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Collections;
+
 @SpringBootApplication
 public class BootcampSantander2023Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BootcampSantander2023Application.class, args);
+		SpringApplication app = new SpringApplication(BootcampSantander2023Application.class);
+		app.setDefaultProperties(Collections.singletonMap("server.port", "8081"));
+		app.run(args);
 	}
 
 }
