98a164bb95e338fa965301969f88656bbcbca04f
Mislaine <a.s.mislaine@gmail.com>
2023-09-29 17:01:08 -0300

    application-prd

--- src/main/java/bootcamp/projeto/BootcampSantander2023Application.java
+++ src/main/java/bootcamp/projeto/BootcampSantander2023Application.java
@@ -1,11 +1,13 @@
 package bootcamp.projeto;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.util.Collections;
 
-
+@OpenAPIDefinition(servers = { @Server(url = "/", description = "Default Server URL")})
 @SpringBootApplication
 public class BootcampSantander2023Application {
 
